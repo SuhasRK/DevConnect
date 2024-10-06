92050633c07970de93276c4cb5c88232064fd059
SuhasRK <suhasrk233@gmail.com>
2024-10-06 23:01:54 +0530

    Adding code for sidenav, header, mainContent and rightNav

--- devConnect_app/src/app/app.component.ts
+++ devConnect_app/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { RightInfoComponent } from './components/right-info/right-info.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,SideNavComponent,MainContentComponent,RightInfoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ devConnect_app/src/app/components/right-info/right-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-right-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './right-info.component.html',
+  styleUrl: './right-info.component.css'
+})
+export class RightInfoComponent {
+
+}
